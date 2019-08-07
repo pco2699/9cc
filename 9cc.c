d4ddbf3a2132c9fc6d9fb464aa5b87c82058ed6c
ktakayama <kazuyuki.takayama@wealthnavi.com>
2019-08-07 13:53:55 +0900

    feature: add AST & ASTGenerator

--- 9cc.c
+++ 9cc.c
@@ -20,6 +20,36 @@ struct Token {
   char *str;
 };
 
+typedef enum {
+  ND_ADD, // +
+  ND_SUB, // -
+  ND_NUM, // 整数
+} NodeKind;
+
+typedef struct Node Node;
+
+struct Node {
+  NodeKind kind;
+  Node *lhs;
+  Node *rhs;
+  int val;
+};
+
+Node *new_node(NodeKind kind, Node *lhs, Node *rhs) {
+  Node *node = calloc(1, sizeof(Node));
+  node->kind = kind;
+  node->lhs = lhs;
+  node->rhs = rhs;
+  return node;
+}
+
+Node *new_node_num(int val) {
+  Node *node = calloc(1, sizeof(Node));
+  node->kind = ND_NUM;
+  node->val = val;
+  return node;
+}
+
 Token *token;
 
 // 入力プログラム
@@ -96,8 +126,9 @@ Token *tokenize(char *p) {
     }
 
     if (isdigit(*p)) {
-      cur = new_token(TK_NUM, cur, p++);
+      cur = new_token(TK_NUM, cur, p);
       cur->val = strtol(p, &p, 10);
+      
       continue;
     }
 
@@ -107,6 +138,44 @@ Token *tokenize(char *p) {
   return head.next;
 }
 
+void gen(Node *node) {
+  if (node->kind == ND_NUM) {
+    printf("  push %d\n", node->val);
+    return;
+  }
+
+  gen(node->lhs);
+  gen(node->rhs);
+
+  printf("  pop rdi\n");
+  printf("  pop rax\n");
+
+  switch (node->kind) {
+    case ND_ADD:
+      printf("  add rax, rdi\n");
+      break;
+    case ND_SUB:
+      printf("  sub rax, rdi\n");
+      break;
+  }
+
+  printf("  push rax\n");
+
+}
+
+Node *expr() {
+  Node *node = new_node_num(expect_number());
+
+  for (;;) {
+    if (consume('+'))
+      node = new_node(ND_ADD, node, new_node_num(expect_number()));
+    else if (consume('-'))
+      node = new_node(ND_SUB, node, new_node_num(expect_number()));
+    else
+      return node;
+  }
+}
+
 
 int main(int argc, char **argv) {
   if (argc != 2) {
@@ -114,23 +183,19 @@ int main(int argc, char **argv) {
     return 1;
   }
 
-  token = tokenize(argv[1]);
   user_input = argv[1];
+  
+  token = tokenize(user_input);
+  Node *node = expr();
 
   printf(".intel_syntax noprefix\n");
   printf(".global main\n");
   printf("main:\n");
-  printf("  mov rax, %d\n", expect_number());
 
-  while (!at_eof()) {
-    if (consume('+')) {
-      printf("  add rax, %d\n", expect_number());
-      continue;
-    }
-    expect('-');
-    printf("  sub rax, %d\n", expect_number());
-  }
+  gen(node);
 
+  printf("  pop rax\n");
   printf("  ret\n");
+
   return 0;
 }
